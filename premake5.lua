2d7732e2bab16fa04a81f36fce9a6b78023cb38b
Torkz <93553904+Torkz@users.noreply.github.com>
2021-12-21 22:14:34 +0100

    added premake file

---
+++ premake5.lua
@@ -0,0 +1,50 @@
+-- premake5.lua
+project "GLFW"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("bin/" .. output_dir .. "/%{prj.name}")
+	objdir ("bin-int/" .. output_dir .. "/%{prj.name}")
+
+	files
+	{
+		"include/GLFW/glfw3.h",
+		"include/GLFW/glfw3native.h",
+		"src/context.c",
+		"src/init.c",
+		"src/input.c",
+		"src/vulkan.c",
+		"src/monitor.c",
+		"src/window.c",
+		"src/platform.c",
+		"src/null_init.c",
+		"src/null_joystick.c",
+		"src/null_monitor.c",
+		"src/null_platform.h",
+		"src/null_window.c"
+	}
+
+	filter "system:windows"
+		buildoptions { "-std=c11", "-lgdi32" }
+		staticruntime "On"
+		systemversion "latest"
+		
+		files
+		{
+			"src/win32_init.c",
+			"src/win32_joystick.c",
+			"src/win32_module.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
+		}
+
+		defines
+		{
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
